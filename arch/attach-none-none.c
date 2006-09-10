de34cd9a9d782fd9ebd3f8777e9479b1e3063be6
Jan Sembera <jan.sembera@innoxia.cz>
2006-09-10 14:41:41 +0200

    Compiling on unsupported architectures now prints correct error message

---
+++ arch/attach-none-none.c
@@ -0,0 +1,1 @@
+#error Unsupported architecture / operating system
--- getarch.sh
+++ getarch.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+check_support() {
+  if [ ! -f arch/attach-$1.[cS] ]; then
+    ARCH="none"
+    OS="none"
+  fi
+}
+
 ARCH=`uname -m`
 if echo $ARCH | grep '^i.86$' >/dev/null 2>/dev/null; then
   ARCH=ia32
 fi
 
 OS=`uname|tr [A-Z] [a-z]`
+
+check_support $ARCH-$OS
 
 if [ "x$1" = "x-os" ]; then
   echo $OS
